1e0c31eb51e8973dd3a82eaebfd320e7b773df9f
Vondarr <vladislavradnov6@gmail.com>
2026-03-22 21:54:50 +0300

    Ряднов 25-ИСТ-1-2 alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,15 +3,41 @@
 #include "alg.h"
 
 uint64_t collatzMaxValue(uint64_t num) {
-  return 1;
+uint64_t result = num;
+    while (num != 1) {
+        if (num % 2 == 0)
+            num /= 2;
+        else
+            num = 3 * num + 1;
+        if (num > result)
+            result = num;
+    }
+    return result;
 }
 
 unsigned int collatzLen(uint64_t num) {
-  return 1;
+    unsigned int result = 1;
+    while (num != 1) {
+        if (num % 2 == 0)
+            num /= 2;
+        else
+            num = num * 3 + 1;
+        result++;
+    }
+    return result;
 }
 
 unsigned int seqCollatz(unsigned int *maxlen,
                         uint64_t lbound,
                         uint64_t rbound) {
-  return 1;
+    unsigned int result = static_cast<unsigned int>(lbound);
+    *maxlen = 0;
+    for (uint64_t i = lbound; i <= rbound; i++) {
+        unsigned int l = collatzLen(i);
+        if (l > *maxlen) {
+            *maxlen = l;
+            result = static_cast<unsigned int>(i);
+        }
+    }
+    return result;
 }
